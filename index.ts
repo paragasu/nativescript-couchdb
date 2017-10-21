4d7e512210c82eb487cd951429da2ccddeca7cf9
paragasu <paragasu@gmail.com>
2017-10-22 02:44:06 +0800

    check response code

--- index.ts
+++ index.ts
@@ -9,31 +9,52 @@ export class CouchDB {
 
   public get(id: string) {
     return new Promise((resolve, reject) => {
-      http.getJSON([this.host, id].join("/")).then(resolve, reject);
+      let url = [this.host, id].join("/");
+      http.getJSON(url).then(response => {
+        if(response.statusCode === 200){
+          resolve(response.content.toJSON());    
+        }else{
+          reject(reponse);
+        }
+      }, err => reject(err));
     })
   }
+
 
   public put(data: Object) {
-    let host = [this.host, data._id].join("/");
+    let params = {
+      url: [this.host, data._id].join("/"),
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      content: JSON.stringify(data)   
+    }
     return new Promise((resolve, reject) => {
-      http.request({
-        url: host,
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        content: JSON.stringify(data)   
-      }).then(resolve, reject);
+      http.request(params).then(response => {
+        if(response.statusCode === 201){
+          resolve(response.content.toJSON());
+        }else{
+          reject(response);
+        }
+      }, err => reject(err));
     })
   }
 
+
   public remove(data: Object) {
-    let host = [this.host, data._id].join("/");
+    let params = {
+      url: [this.host, data._id].join("/"),
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      content: JSON.stringify(data)   
+    }
     return new Promise((resolve, reject) => {
-      http.request({
-        url: host,
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-        content: JSON.stringify(data)   
-      }).then(resolve, reject);
+      http.request(params).then(response => {
+        if(response.statusCode === 200){
+          resolve(response.content.toJSON());
+        }else{
+          reject(response);
+        }
+      }, err => reject(err));
     })
   }
 }
