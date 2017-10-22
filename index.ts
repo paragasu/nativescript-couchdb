631710ef44bd20eb53d4f9c95aa2b51596e02a9f
paragasu <paragasu@gmail.com>
2017-10-22 10:35:40 +0800

    add api support for alldocs

--- index.ts
+++ index.ts
@@ -57,4 +57,28 @@ export class CouchDB {
       }, err => reject(new Error(err)));
     })
   }
+
+
+  private buildRequestParams(data: Object) {
+    let keys = Object.keys(data);
+    let args: string[] = "";
+    keys.forEach(key => {
+      let value = [key, encodeURI(data[key])].join("=");
+      args.push(value);
+    })
+    return "?" + args;
+  }
+
+
+  public allDocs(data: Object) {
+    let url = [this.url, "_all_docs", this.buildRequestParams(data)].join("/");
+      http.getJSON(url).then(response => {
+        if(response.statusCode === 200){
+          resolve(response.content.toJSON());    
+        }else{
+          reject(new Error(reponse));
+        }
+      }, err => reject(new Error(err)));
+    })
+  }
 }
