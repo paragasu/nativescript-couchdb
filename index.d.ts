390c4429fd47a5e0ce11ad61a85c95f31b7449b7
paragasu <paragasu@gmail.com>
2017-10-23 13:49:41 +0800

    update definition file

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 export interface Data {
     _id: string;
     _rev?: string;
-    updatedAt: string;
-    updatedBy: string;
-    createdAt: string;
-    createdBy: string;
+    updatedAt?: string;
+    updatedBy?: string;
+    createdAt?: string;
+    createdBy?: string;
 }
 export interface QueryParams {
     startkey?: string;
@@ -12,6 +12,19 @@ export interface QueryParams {
     start_key?: string;
     end_key?: string;
     limit?: number;
+    conflicts?: boolean;
+    descending?: boolean;
+    endkey_docid?: string;
+    end_key_doc_id?: string;
+    startkey_docid?: string;
+    start_key_doc_id?: string;
+    include_docs?: boolean;
+    inclusive_end?: boolean;
+    key?: string;
+    keys?: string;
+    skip?: number;
+    stale?: string;
+    update_req?: boolean;
 }
 export declare class CouchDB {
     private host;
--- index.ts
+++ index.ts
@@ -3,10 +3,10 @@ import * as http from "http";
 export interface Data {
   _id: string,
   _rev?: string,
-  updatedAt: string,
-  updatedBy: string,
-  createdAt: string,
-  createdBy: string
+  updatedAt?: string,
+  updatedBy?: string,
+  createdAt?: string,
+  createdBy?: string
 }
 
 export interface QueryParams {
@@ -15,6 +15,19 @@ export interface QueryParams {
   start_key?: string,
   end_key?: string,
   limit?: number
+  conflicts?: boolean,
+  descending?: boolean, 
+  endkey_docid?: string,
+  end_key_doc_id?: string,
+  startkey_docid?: string,
+  start_key_doc_id?: string,
+  include_docs?: boolean,
+  inclusive_end?: boolean,
+  key?: string,
+  keys?: string,
+  skip?: number,
+  stale?: string,
+  update_req?: boolean
 }
 
 export class CouchDB {
