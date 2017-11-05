5f8117e0c2954a6871ea93eb67b2c14aa56b396e
paragasu <paragasu@gmail.com>
2017-11-05 22:09:04 +0800

    fix keys type

--- index.d.ts
+++ index.d.ts
@@ -18,7 +18,7 @@ export interface QueryParams {
     include_docs?: boolean;
     inclusive_end?: boolean;
     key?: string;
-    keys?: string;
+    keys?: string[];
     skip?: number;
     stale?: string;
     update_req?: boolean;
